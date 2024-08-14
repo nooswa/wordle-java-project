58d61ec99c6f102520e0589fc4f7808a01233ec6
Noor <noorfswadi@gmail.com>
2024-08-14 22:12:35 +1200

    Update Input.java

--- UserInput.java
+++ UserInput.java
@@ -1,19 +1,41 @@
 
-package wordle;
+package com.mycompany.wordleproject;
 
 import java.util.Scanner;
+import java.util.List;
 
-public class UserInput {
-    
-    
-    public String getUserInput() {
-        System.out.println("Enter a 5 Letter Word");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+public class InputHandler {
+    private Scanner scanner;
+    private WordList wordList;
+    private static final int WORD_LENGTH = 5;
+
+    // Constructor 
+    public InputHandler(WordList wordList) {
+        this.scanner = new Scanner(System.in);
+        this.wordList = wordList;
     }
-    
 
-    
+    // Method to get the user's input
+    public String getUserInput() {
+        String input;
+        do {
+            System.out.println("Enter Your Guess: ");
+            input = scanner.nextLine().trim();
+            input = input.toUpperCase();//Words in uppercase
+        } while (!isValidInput(input));
+        return input;
+    }
 
-    
+    // Check if the word is valid
+    private boolean isValidInput(String input) {
+        if (input.length() != WORD_LENGTH) {
+            System.out.println("Your guess must be a 5 letter word. Please try again.");
+            return false;
+        }
+        if (!wordList.getValidWords().contains(input)) { //error here
+            System.out.println("Input is not a valid word.");
+            return false;
+        }
+        return true;
+    }
 }
