ebf9c7c6e04fdbc468cd1a64a69e290eaceb31d3
larissagoh <149050033+larissagoh@users.noreply.github.com>
2024-08-14 14:48:26 +1200

    Update WordList.java

--- WordList.java
+++ WordList.java
@@ -1,18 +1,64 @@
 
 package wordle;
+
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList; // import the ArrayList class
-
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WordList {
-    
-    
-    private void  loadWords() {
-        BufferedReader br = new BufferedReader(new FileReader("./resources/word_list.rtd"));
-        ArrayList<String> words = new ArrayList<String>();
-        //words.add(br);
+    private String secretWord;  
+
+    public String getSecretWord() {
+        return secretWord;
     }
-    
+
+    public void selectRandomWord() {
+        BufferedReader br = null;
+        ArrayList<String> words = new ArrayList<>();
+
+        try {
+            br = new BufferedReader(new FileReader("./resources/Word List.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] arrOfStr = line.split(" ");
+                words.addAll(Arrays.asList(arrOfStr));
+            }
+
+            Random random = new Random();
+            if (!words.isEmpty()) {
+                secretWord = words.get(random.nextInt(words.size()));
+            } else {
+                Logger.getLogger(WordList.class.getName()).log(Level.SEVERE, "The word list is empty.");
+            }
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(WordList.class.getName()).log(Level.SEVERE, "File not found.", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(WordList.class.getName()).log(Level.SEVERE, "Failed to read the file.", ex);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(WordList.class.getName()).log(Level.SEVERE, "idk what to put here", ex);
+                }
+            }
+        }
+    }
+
+    public void initialiseSecretWord() {
+     selectRandomWord();
+    }
 }
+
+    
+  
+
+
